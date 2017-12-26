31a05ef30cf9669a9cf5c397810fecb94b504740
Yusuke arakawa <yusu.arakawa@gmail.com>
2017-12-26 17:21:48 +0900

    first commit

---
+++ src/main/java/com/jasper/Jasperboot1Application.java
@@ -0,0 +1,12 @@
+package com.jasper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jasperboot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jasperboot1Application.class, args);
+	}
+}
