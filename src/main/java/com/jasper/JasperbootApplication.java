d564c011d0fc35ec81d201781e896d78c6842915
Yusuke arakawa <yusu.arakawa@gmail.com>
2017-12-27 09:14:43 +0900

    a little change

--- src/main/java/com/jasper/JasperbootApplication.java
+++ src/main/java/com/jasper/JasperbootApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Jasperboot1Application {
+public class JasperbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Jasperboot1Application.class, args);
+		SpringApplication.run(JasperbootApplication.class, args);
 	}
 }
--- src/test/java/com/jasper/JasperbootApplicationTests.java
+++ src/test/java/com/jasper/JasperbootApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Jasperboot1ApplicationTests {
+public class JasperbootApplicationTests {
 
 	@Test
 	public void contextLoads() {
