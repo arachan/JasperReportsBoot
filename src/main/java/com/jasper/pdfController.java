784e8d501de16ff7341e6a3f07199c9f71c9665c
Yusuke arakawa <yusu.arakawa@gmail.com>
2018-01-12 09:49:19 +0900

    remove JasperReport Field. font extension problem fixed.

--- src/main/java/com/jasper/pdfController.java
+++ src/main/java/com/jasper/pdfController.java
@@ -3,7 +3,6 @@ package com.jasper;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
-
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.sf.jasperreports.engine.JRDataSource;
@@ -25,18 +25,18 @@ import net.sf.jasperreports.engine.JasperReport;
 
 @RestController
 public class pdfController {
-	private JasperReport report;
-	
-    @Autowired
+
+	@Autowired
     ApplicationContext context;
-	
+    	
 	@GetMapping(path = "pdf/{jrxml}")
-    public void getPdf(@PathVariable String jrxml,@RequestParam(name = "name", defaultValue = "山田") String name, HttpServletResponse response) throws Exception {
+	@ResponseBody
+    public void getPdf(@PathVariable String jrxml ,@RequestParam(name = "name", defaultValue = "山田") String name, HttpServletResponse response) throws Exception {    	
 		//Get JRXML template from resources folder
 		Resource resource = context.getResource("classpath:jasperreports/"+jrxml+".jrxml");
         //Compile to jasperReport
         InputStream inputStream = resource.getInputStream();
-		this.report=JasperCompileManager.compileReport(inputStream);
+        JasperReport report=JasperCompileManager.compileReport(inputStream);		
 		//Parameters Set
         Map<String, Object> params = new HashMap<>();
         params.put("name", name);
